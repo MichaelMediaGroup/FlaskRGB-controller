ed6718781866d8c881ce65803df34bf2a8d0b59b
Michael Parker <michaelrbparker@protonmail.com>
2024-07-01 19:35:19 +0100

    FLask LED controller

---
+++ app.py
@@ -0,0 +1,43 @@
+from flask import Flask, render_template, request, redirect, url_for
+import time
+import time
+import board
+import neopixel
+
+app = Flask(__name__)
+
+num_pixels = 100
+pixel_pin = board.D18
+ORDER = neopixel.RGB
+pixels = neopixel.NeoPixel(
+    pixel_pin, num_pixels, brightness=0.2, auto_write=False, pixel_order=ORDER
+)
+
+def setlight(r,g,b):
+    pixels.fill((0,0,0))
+    pixels.show()
+    pixels.fill((r,g,b))
+    pixels.show()
+
+
+app = Flask(__name__)
+
+def hex_to_rgb(value):
+    value = value.lstrip('#')
+    lv = len(value)
+    return tuple(int(value[i:i + lv // 3], 16) for i in range(0, lv, lv // 3))
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        colour = request.form['colour_in']
+        setlight(hex_to_rgb(colour))
+    return render_template('index.html')
+
+@app.route('/submit-colour',methods=['POST'])
+def submit_colour():
+    colour = request.form['colour_in']
+    return colour
+
+if __name__ == '__main__':
+    app.run(debug=True,host='0.0.0.0')
